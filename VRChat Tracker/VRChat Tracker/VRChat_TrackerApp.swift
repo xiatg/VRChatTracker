f3e06b293665a1a487a757e83cffa19ccd93c42a
Tongguang Watanabe Xia <ghbq674521@163.com>
2023-03-08 21:50:42 -0600

    feat: rating alert after 3 opens, document draft

--- VRChat Tracker/VRChat Tracker/VRChat_TrackerApp.swift
+++ VRChat Tracker/VRChat Tracker/VRChat_TrackerApp.swift
@@ -11,21 +11,51 @@ import SwiftUI
 struct VRChat_TrackerApp: App {
     @StateObject var client = VRChatClient()
     
-    init() {
-        registration()
-    }
+    let defaults = UserDefaults.standard
+    
+    @State private var showRating = false
     
     var body: some Scene {
         WindowGroup {
             if (client.isLoggedIn) {
                 NavigationView(client: client)
+                    .onAppear() {
+                        registration()
+                    }
+                    .alert(isPresented: self.$showRating) {
+                        Alert(title: Text("Rate this App in the App Store!"),
+                              message: Text("Please? 🥺"),
+                              dismissButton: .default(Text("Okay")))
+                    }
             } else {
                 LoginView(client: client)
+                    .onAppear() {
+                        registration()
+                    }
+                    .alert(isPresented: self.$showRating) {
+                        Alert(title: Text("Rate this App in the App Store!"),
+                              message: Text("Please? 🥺"),
+                              dismissButton: .default(Text("Okay")))
+                    }
             }
         }
     }
     
     func registration() {
+        defaults.register(defaults: ["openTime": 0])
+        
+        var openTime = defaults.integer(forKey: "openTime")
+        openTime += 1
+        defaults.set(openTime, forKey: "openTime")
+        
+        //Debug
+        print("open time: \(openTime)")
+        
+        if (openTime == 3) {
+            self.showRating = true
+            
+            print("show rating!! \(self.showRating)")
+        }
         
     }
 }
