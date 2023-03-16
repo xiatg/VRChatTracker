0b1675b597dd903cc9b09a258e9d42b2f301a7a7
Tongguang Watanabe Xia <ghbq674521@163.com>
2023-03-15 22:24:07 -0500

    fix: remove splash screen, rating notification

--- VRChat Tracker/VRChat Tracker/VRChat_TrackerApp.swift
+++ VRChat Tracker/VRChat Tracker/VRChat_TrackerApp.swift
@@ -11,53 +11,17 @@ import SwiftUI
 struct VRChat_TrackerApp: App {
     @StateObject var client = VRChatClient()
     
-    let defaults = UserDefaults.standard
-    
-    @State private var showRating = false
-    @State private var showSplash = true
-    
     var body: some Scene {
         
         // check if the user is already logged in
         // yes: navigation view
         // no: login page view
         WindowGroup {
-            
-            if (showSplash) {
-                SplashView()
-                    .onTapGesture {
-                        showSplash = false
-                    }
-                    .onAppear() {
-                        registration()
-                    }
-                    .alert(isPresented: self.$showRating) {
-                        Alert(title: Text("Rate this App in the App Store!"),
-                              message: Text("Please? 🥺"),
-                              dismissButton: .default(Text("Okay")))
-                    }
-            } else if (client.isLoggedIn) {
+            if (client.isLoggedIn) {
                 NavigationView(client: client)
             } else {
                 LoginView(client: client)
             }
-        }
-    }
-    
-    func registration() {
-        defaults.register(defaults: ["openTime": 0])
-        
-        var openTime = defaults.integer(forKey: "openTime")
-        openTime += 1
-        defaults.set(openTime, forKey: "openTime")
-        
-        //Debug
-        print("open time: \(openTime)")
-        
-        if (openTime == 3) {
-            self.showRating = true
-            
-            print("show rating!! \(self.showRating)")
         }
     }
 }
